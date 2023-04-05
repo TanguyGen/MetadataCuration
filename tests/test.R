749ee975746730a3513396a683a00119ab94f2a5
TanguyGen <tanguy.genthon22@gmail.com>
2023-04-05 12:54:38 +0200

    Update test.R

--- tests/test.R
+++ tests/test.R
@@ -1,13 +1,13 @@
 library(metadig)
 library(xml2)
-library(stringr)
+setwd("C:/Users/tangu/Desktop/Stage_meta/Curation/Curation/tests")
 
-checkFile <- "C:/Users/tangu/Desktop/Stage_meta/Curation/Curation/Programs/dataset_title_length-check.xml"
-metadataFile <- "C:/Users/tangu/Desktop/Stage_meta/Curation/Curation/Programs/example_EML.xml"
-metadataFile <- "C:/Users/tangu/Desktop/Stage_meta/Curation/Curation/Programs/Assessing_the_importance_of_field_margins_for_bat_species_and_communities_in_intensive_agricultural_landscapes_-_Data.xml"
+metadataFile <- "Reef_Life_Survey_Fish_Mediterranean_sample.xml"
 
+checkFile <- "abstract.100.words.xml"
 results <- runCheck(checkFile, metadataFile)
 results
 
-
-checkFile <- "C:/Users/tangu/Desktop/Stage_meta/Curation/Curation/Programs/abstract.100.words-2.0.0.xml"
+checkFile <- "Publication_date_check.xml"
+results <- runCheck(checkFile, metadataFile)
+results
